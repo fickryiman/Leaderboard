2338a68cbd982a517c59dde47bdc5459ee5887fb
Fickry Iman <fickry.iman@gmail.com>
2023-04-11 22:16:22 +0700

    remove scoreAdd.js & ESLint fixes

--- src/index.js
+++ src/index.js
@@ -13,7 +13,7 @@ formInput.addEventListener('submit', (e) => {
   const user = document.getElementById('user').value;
   const score = document.getElementById('score').value;
 
-  fetch(`https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/hs3Uvn8oXxiNOYVsoXdL/scores/`, {
+  fetch('https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/hs3Uvn8oXxiNOYVsoXdL/scores/', {
     method: 'POST',
     body: JSON.stringify({
       user,
@@ -21,14 +21,12 @@ formInput.addEventListener('submit', (e) => {
     }),
     headers: {
       'Content-type': 'application/json; charset=UTF-8',
-    }
+    },
   })
-  .then(response => {
-    return response.json();
-  })
-  .catch(error => {
-    console.error('Error', error);
-  })
-  
+    .then((response) => response.json())
+    .catch((error) => {
+      throw new Error(`HTTP error! status: ${error}`);
+    });
+
   formInput.reset();
 });
